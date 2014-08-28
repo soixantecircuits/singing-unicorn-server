16f144c965e01b1f5ddf2f90b368048738d51efc
Arthur Battut <arthur.battut@gmail.com>
2014-08-28 11:37:58 +0200

    add google analytics

--- client/example.social.js
+++ client/example.social.js
@@ -9,3 +9,11 @@
   js.src = "//connect.facebook.net/fr_CA/sdk.js#xfbml=1&appId=782323101787355&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-XXXXXXXXX-Y', 'auto');
+ga('send', 'pageview');
