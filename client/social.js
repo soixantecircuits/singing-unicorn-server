a6f30da638a9ab6564b54b4dcbd85b2128a41489
Arthur Battut <arthur.battut@gmail.com>
2014-08-29 10:13:40 +0200

    Put socials ids in settings json file

--- client/social.js
+++ client/social.js
@@ -1,3 +1,6 @@
+var googleId = Meteor.settings.google.id,
+    fbAppId = Meteor.settings.facebook.appId;
+
 // Twitter
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
@@ -6,14 +9,14 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_CA/sdk.js#xfbml=1&appId=782323101787355&version=v2.0";
+  js.src = "//connect.facebook.net/fr_CA/sdk.js#xfbml=1&appId="+fbAppId+"&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
+// Google
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-XXXXXXXXX-Y', 'auto');
+ga('create',googleId, 'auto');
 ga('send', 'pageview');
